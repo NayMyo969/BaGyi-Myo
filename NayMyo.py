d68a12a1363028ec7fe53c3f1f4e3bb5ce7693f0
NayMyo969 <70550937+NayMyo969@users.noreply.github.com>
2020-09-13 01:23:47 +0630

    Create NayMyo.py

---
+++ NayMyo.py
@@ -0,0 +1,64 @@
+import os
+import sys
+import time
+
+os.system('clear')
+time.sleep(1)
+os.system('cowsay eyes')
+os.system('figlet Nay Myo')
+
+print "========================================"
+print " Author   : Nay Myo"
+print " Facebook : Nay Myo(Bagyi Myo)"
+print " Github   : https://github.com/NayMyo969"
+print " Blog     : naymyo15689.blogspot.com"    
+print "========================================"
+print
+time.sleep(1)
+print "[+] Menu Nay Myo [+]"
+print "[1] Nay Myo Brute Force -A"
+print "[2] Nay Myo Brute Force -B"
+print "[3] Nay Myo Fb-Video Downloader"
+print "[4] Nay Myo Instagram Video Downloader"
+print "[5] Install pkg"
+pilih = raw_input('[?] Choice :  ' )
+time.sleep(1)
+if pilih == "1":
+        os.system('git clone https://github.com/NayMyo969/NayMyo-M')
+        print "[+] Nay Myo Brute Force -A"
+        time.sleep(1)
+        print "[+] Call Directory $ cd NayMyo-M"
+        time.sleep(1)
+        print "[+] For Running   $ python2 NayMyo"
+elif pilih == "2":
+        os.system('git clone https://github.com/NayMyo969/MYANMAR')
+        print "[+] Nay Myo Brute Force -B"
+        time.sleep(1)
+        print "[+] Call Directory $ cd MYANMAR"
+        time.sleep(1)
+        print "[+] For Running   $ python2 NayMyo.py"
+elif pilih == "3":
+        os.system('git clone https://github.com/NayMyo969/BagyiMyo-M')
+        print "[+] Call Directory $ cd BagyiMyo-M"
+        time.sleep(1)
+        print "[+] For Running   $ pip install -r requirement.txt"
+        time.sleep(1)
+        print "[+] For Running   $ python NayMyo.py"
+elif pilih == "4":
+        os.system('git clone https://github.com/NayMyo969/BagyiMyo-M')
+        print "[+] Call Directory $ cd BagyiMyo-M"
+        time.sleep(1)
+        print "[+] For Running   $ pip install -r requirement.txt"
+        time.sleep(1)
+        print "[+] For Running   $ python NayMyo.py"
+elif pilih == "5":
+        os.system('apt update && apt upgrade')
+        os.system('pkg install python')
+        os.system('pkg install python2')
+        os.system('pkg install nano')
+        os.system('pkg install git')
+        os.system('pkg install figlet')
+        os.system('pkg install cowsay')
+        os.system('pip2 install requests')
+        os.system('pip2 install mechanize')
+           
